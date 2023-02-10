47c13f78e43df040215369b173f074e131cadea3
Lan <darkpanda45@gmail.com>
2023-02-09 21:33:05 -0500

    submitted, missed some points

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -11,7 +11,7 @@ const AllocationForm = (props) => {
     const submitEvent = () => {
 
             if(cost > remaining) {
-                alert("The value cannot exceed remaining funds  £"+remaining);
+                alert("You cannot reduce the budget value lower than the spending.");
                 setCost("");
                 return;
             }
--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,10 +1,21 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 const Budget = () => {
     const { budget } = useContext(AppContext);
+    const [cost, setCost] = useState('');
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>
+                Budget: £<input
+                        required='required'
+                        type='number'
+                        id='cost'
+                        value={cost}
+                        style={{ marginLeft: '2rem' , size: 10}}
+                        onChange={(event) => setCost(event.target.value)}>
+                        </input>
+                
+            </span>
         </div>
     );
 };
